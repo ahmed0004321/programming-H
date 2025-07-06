375872cb9b11f0cdea6ad3a94bf10a51531bc63c
ahmed0004321 <oasifrikto@gmail.com>
2025-07-06 17:22:15 +0600

    added anilife

---
+++ introduction to javascript/variable.js
@@ -0,0 +1,24 @@
+/* 5 things needed to declare a variable*/
+var weight = 38;
+var price = 2005;
+var age = 45;
+
+console.log(age);
+console.log(price);
+
+
+
+
+
+
+
+
+
+
+/* variable declare er time e first e declare korlam var age = 34; but pore jodi
+ai age value re change korte chai taile r pore var lekhar dorkar nai 
+pore just age = changed value ; dilei hobe;*/
+
+var age = 44;
+age = 55;
+console.log(age);
